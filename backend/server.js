795445da0990eac52c57ecf8453e63b04b53bcb9
Shubh Kesharwani <kesharwanishubh17@gmail.com>
2026-04-03 14:24:42 +0530

    backend

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config' 
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
+import cartRouter from './routes/cartRoute.js'
+import orderRouter from './routes/orderRoute.js'
+
+//App config
+const app = express()
+const port = process.env.PORT || 5000
+connectDB();
+connectCloudinary();
+// middlewares
+app.use(express.json())
+app.use(cors({
+  origin: [
+    "https://cart-q5kr.vercel.app",
+    "https://cart-6288.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}))
+
+
+// api endpoints
+app.use('/api/user', userRouter);
+app.use('/api/product', productRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
+app.get('/', (req, res) => {
+    res.send("API working")
+})
+
+app.listen(port, ()=> console.log('Server started on port: ' + port));
